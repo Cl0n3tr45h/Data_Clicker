427fc4c51150758eb43e49cda25a425389aec1ea
HanterGER <hanswuehling@gmail.com>
2020-04-03 17:07:48 +0200

    Datenkäfer erstellt oder so

--- src/buyers/Buyers_Template.java
+++ src/buyers/Buyers_Template.java
@@ -1,5 +1,29 @@
 package buyers;
 
-public class Buyers_Template {
+import player.Player;
 
+public class Buyers_Template 
+{
+	private String name;
+	private int level;
+	private int preis;
+	private int gewinn;
+	
+	public Datenkauf(String name, int preis, int gewinn)
+	{
+		this.name = name;
+		this.level = 1;
+		this.preis = preis;
+		this.gewinn = gewinn;
+	}
+	public void kauf()
+	{
+		if(dataAmount >= preis)
+		{
+			level = level + 1;
+			dataAmount = dataAmount - preis;
+			preis = preis * 2;
+			amountOfMoney = amountOfMoney + gewinn;
+		}	
+	}
 }
